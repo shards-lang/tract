4b5f22863c57bdd1127c41df1d7bad3184472abf
Mathieu Poumeyrol <mathieu@poumeyrol.fr>
2023-02-08 10:33:47 +0100

    merge test into src/lib

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -2035,3 +2035,19 @@ pub mod internal {
     pub use std::hash::Hash;
     pub use tract_data::internal::*;
 }
+
+#[test]
+fn crasher_monterey_matmul() {
+    use crate::internal::*;
+    use tract_ndarray::prelude::*;
+    use crate::ops::matmul::*;
+    let mut model = TypedModel::default();
+    let wire = model.add_source("input", f32::fact(&[1usize,1])).unwrap();
+    let a = model.add_const("a", Tensor::zero::<f32>(&[2,1]).unwrap().into_arc_tensor()).unwrap();
+    let axes = MatMulAxes::default_for_rank(2).transposing(false, true, true);
+    let op = MatMul { axes };
+    let wire = model.wire_node("conv", op, &[a, wire]).unwrap()[0];
+    model.set_output_outlets(&[wire]).unwrap();
+    let decluttered = model.into_decluttered().unwrap();
+    let optimized = decluttered.into_optimized().unwrap();
+}
